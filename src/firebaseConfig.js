cefa79bfece8720e5e9241cde1c11e481c9611c2
jewelran <mdjewelrana6487@gmail.com>
2020-10-31 01:27:46 +0600

    fire-auth

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDuH7Vho27U2R0JIERVjwIkbm9-r6T5jKE",
+    authDomain: "freelancer-jewel.firebaseapp.com",
+    databaseURL: "https://freelancer-jewel.firebaseio.com",
+    projectId: "freelancer-jewel",
+    storageBucket: "freelancer-jewel.appspot.com",
+    messagingSenderId: "455333987458",
+    appId: "1:455333987458:web:ff803c1a3025fdd19a609b"
+  };
+  export default firebaseConfig;
